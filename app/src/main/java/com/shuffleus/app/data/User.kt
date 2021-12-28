d8042069701140b182f0f128b0f005ef7a00704e
KleprlikJan <kleprlikjan@gmail.com>
2021-12-28 20:53:44 +0100

    Working base

--- app/src/main/java/com/shuffleus/app/data/User.kt
+++ app/src/main/java/com/shuffleus/app/data/User.kt
@@ -1,9 +1,5 @@
 package com.shuffleus.app.data
 
-import com.squareup.moshi.JsonClass
-
-
-@JsonClass(generateAdapter = true )
 data class User(val name: String,
                 val surname: String,
                 val isActive: Boolean,
