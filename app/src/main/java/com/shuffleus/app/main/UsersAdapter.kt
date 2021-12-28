c84bf41235b241ba8370bf33a7e8fb2e0071e27a
KleprlikJan <kleprlikjan@gmail.com>
2021-12-29 00:15:31 +0100

    Add working settings base

--- app/src/main/java/com/shuffleus/app/main/UsersAdapter.kt
+++ app/src/main/java/com/shuffleus/app/main/UsersAdapter.kt
@@ -3,8 +3,12 @@ package com.shuffleus.app.main
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 import com.shuffleus.app.R
 import com.shuffleus.app.data.User
 
@@ -33,12 +37,20 @@ class UsersAdapter(users: List<User>): RecyclerView.Adapter<UserViewHolder>(){
 
 class UserViewHolder(view: View): RecyclerView.ViewHolder(view){
 
+    var view = view
     var txtName: TextView = view.findViewById(R.id.txt_name_def)
     var txtSurname: TextView = view.findViewById(R.id.txt_surname_def)
+    var imgAvatar: ImageView = view.findViewById(R.id.img_avatar)
 
     fun bind(user: User){
         txtName.text = user.name
         txtSurname.text = user.surname
+        Glide.with(view)
+            .load("https://picsum.photos/64/64")
+            .apply(
+                RequestOptions.circleCropTransform()
+                .signature(ObjectKey("${user.name}_${user.surname}")))
+            .into(imgAvatar)
     }
 
 }
--- app/src/main/java/com/shuffleus/app/settings/UsersAdapter.kt
+++ app/src/main/java/com/shuffleus/app/settings/UsersAdapter.kt
@@ -3,11 +3,15 @@ package com.shuffleus.app.settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.signature.ObjectKey
 import com.shuffleus.app.R
 import com.shuffleus.app.data.User
+import java.security.Signature
 
 class UsersAdapter(users: List<User>): RecyclerView.Adapter<UserViewHolder>(){
 
@@ -33,15 +37,21 @@ class UsersAdapter(users: List<User>): RecyclerView.Adapter<UserViewHolder>(){
 }
 
 class UserViewHolder(view: View): RecyclerView.ViewHolder(view){
-
+    val view = view
     var txtName: TextView = view.findViewById(R.id.txt_name)
     var txtSurname: TextView = view.findViewById(R.id.txt_surname)
-    var btnIsActive: Button = view.findViewById(R.id.btn_isActive)
+    var btnIsActive: CheckBox = view.findViewById(R.id.btn_isActive)
+    var imgAvatar: ImageView = view.findViewById(R.id.img_avatar)
 
     fun bind(user: User){
         txtName.text = user.name
         txtSurname.text = user.surname
-        btnIsActive.isEnabled = user.isActive
+        btnIsActive.isChecked = user.isActive
+        Glide.with(view)
+            .load("https://picsum.photos/64/64")
+            .apply(RequestOptions.circleCropTransform()
+                .signature(ObjectKey("${user.name}_${user.surname}")))
+            .into(imgAvatar)
     }
 
 }
